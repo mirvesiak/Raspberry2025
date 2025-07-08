a60aa03225ab1ea9406529ef0ae94b5fad6bcf6c
mirvesiak <mateuszrozm@gmail.com>
2025-07-08 21:12:57 +0200

    header fix

--- include/camera_stream.hpp
+++ include/camera_stream.hpp
@@ -1,7 +1,2 @@
-static int wsConnect(mg_connection*, void*);
-static void wsMessage(mg_connection *conn, int, char *data, size_t len, void*);
-
-static int streamHandler(struct mg_connection *conn, void * /*cbdata*/);
-
 void start_mjpeg_server();
 void stop_mjpeg_server();
