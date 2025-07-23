7b11a40b4fdc4f28cd66c0720bea9d664b7dea26
mirvesiak <mateuszrozm@gmail.com>
2025-07-23 16:23:27 +0200

    bug fix

--- include/camera_stream.hpp
+++ include/camera_stream.hpp
@@ -5,5 +5,5 @@ extern std::atomic<int> joystick_angle;
 extern std::atomic<int> joystick_distance;
 extern std::atomic<bool> isGrabbing;
 
-void start_mjpeg_server();
+void start_mjpeg_server(bool stream);
 void stop_mjpeg_server();
